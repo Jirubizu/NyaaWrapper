2034f45bcfda12def8a75c9f43ede8d7bd13cede
jirubizu <zilvysdovydas@yahoo.co.jp>
2021-03-30 00:01:00 +0100

    Added basic test to make sure it still works

--- NyaaWrapper.Test/Program.cs
+++ NyaaWrapper.Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace NyaaWrapper.Test
 {
@@ -6,7 +8,26 @@ namespace NyaaWrapper.Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Wrapper nyaaWrapper = new();
+            List<NyaaTorrentStruct> entries =
+                nyaaWrapper.GetEntries(new QueryOptions {Search = "kimetsu no yaiba"}).Result;
+            StringBuilder stringBuilder = new();
+            foreach (NyaaTorrentStruct torrent in entries)
+            {
+                stringBuilder.AppendLine($"Name: {torrent.Name}");
+                stringBuilder.AppendLine($"Id: {torrent.Id}");
+                stringBuilder.AppendLine($"Category: {torrent.Category}");
+                stringBuilder.AppendLine($"URL: {torrent.Url}");
+                stringBuilder.AppendLine($"Download URL: {torrent.DownloadUrl}");
+                stringBuilder.AppendLine($"Magnet: {torrent.Magnet}");
+                stringBuilder.AppendLine($"Size: {torrent.Size}");
+                stringBuilder.AppendLine($"Date: {torrent.Date}");
+                stringBuilder.AppendLine($"Seeders: {torrent.Seeders}");
+                stringBuilder.AppendLine($"Leechers: {torrent.Leechers}");
+                stringBuilder.AppendLine($"Completed Downloads: {torrent.CompletedDownloads}");
+                stringBuilder.AppendLine("----------------------------------------");
+            }
+            Console.WriteLine(stringBuilder);
         }
     }
 }
